7349156c90b87329c74ebc629124c60fa32da55a
zhangsichao <zhangsichao@bigo.sg>
2018-06-05 17:13:24 +0800

    首页列表实现懒加载

--- app/src/main/java/com/coyoal/zsc/cnode/views/base/BaseFragment.kt
+++ app/src/main/java/com/coyoal/zsc/cnode/views/base/BaseFragment.kt
@@ -16,20 +16,41 @@ abstract class BaseFragment<V, T: BasePresenter<V>>: Fragment() {
     protected lateinit var mPresenter: T
     private var rootView: View? = null
 
+    private var isUIPrepared = false
+    private var isInited = false
+
     @Suppress("UNCHECKED_CAST")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(getLayout(), container, false)
+        isUIPrepared = true
         mPresenter = initPresenter()
         mPresenter.attach(this as V)
         initViews(rootView)
-        initData()
         initEvents()
+        lazyLoad()
         return rootView
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         mPresenter.detach()
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        if (isVisibleToUser) {
+            lazyLoad()
+        }
+    }
+
+    // 懒加载
+    private fun lazyLoad() {
+        // setUserVisibleHint()会在onCreateView()之前调用一次，isUIPrepared防止此时加载数据
+        // isInited加载过数据后无需重复加载
+        if (userVisibleHint && isUIPrepared && !isInited) {
+            initData()
+            isInited = true
+        }
     }
 
     @LayoutRes
